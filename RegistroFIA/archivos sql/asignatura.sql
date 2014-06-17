f6ea30e5a5236e44cab676dde60f15428865b770
danielpunx1 <daniel_punx1@hotmail.com>
2014-06-17 03:12:08 -0600

    Agregando los proc y datos de las tablas

--- RegistroFIA/archivos sql/asignatura.sql
+++ RegistroFIA/archivos sql/asignatura.sql
@@ -49,3 +49,30 @@ execute sp_asignatura('FDE115','FUNDAMENTOS DE ECONOMIA',null,null,null,4,null,n
 
 -- FIN DE LOS DATOS DE LA TABLA ASIGNATURA
 
+--procedimiento que retorna las asignaturas a usar para los prerrequisitos
+
+CREATE OR REPLACE PROCEDURE sp_asignatura_retornar
+(
+    datos OUT SYS_REFCURSOR
+)
+AS
+BEGIN
+    open datos for
+    select codigo_asignatura from asignatura where VISTO_BUENO != 0 and APROBACION != 0;
+END sp_asignatura_retornar;
+
+--retorna los prerrequisitos para eliminarlos
+
+CREATE OR REPLACE PROCEDURE sp_pre_ret_eliminar
+(
+    materia IN PRERREQUISITO.CODIGO_ASIGNATURA%type,
+    datos OUT SYS_REFCURSOR
+)
+AS
+BEGIN
+    open datos for
+    select materia_rrequisito from prerrequisito where CODIGO_ASIGNATURA = materia;
+END sp_pre_ret_eliminar;
+
+
+
