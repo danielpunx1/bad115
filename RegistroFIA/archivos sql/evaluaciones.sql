afbb4d8307740a4054992b8986efc9af17cb5391
danielpunx1 <daniel_punx1@hotmail.com>
2014-06-17 01:39:25 -0600

    Agregando los proc y datos de las tablas

--- RegistroFIA/archivos sql/evaluaciones.sql
+++ RegistroFIA/archivos sql/evaluaciones.sql
@@ -5,13 +5,13 @@ INCREMENT BY 1;
 
 CREATE OR REPLACE PROCEDURE sp_evaluaciones
 (
-  codigo_asignatura1 in EVALUACIONES.CODIGO_ASIGNATURA%type, evaluacion1 in EVALUACIONES.EVALUACION%type,
+  codigo_asignatura1 in EVALUACIONES.CODIGO_ASIGNATURA%type, 
   porcentaje_evaluacion1 in EVALUACIONES.PORCENTAJE_EVALUACION%type
 )
 IS
 BEGIN
   insert into evaluaciones (id_evaluacion,codigo_asignatura,evaluacion,porcentaje_evaluacion)
-  values (id_evaluaciones.nextval,codigo_asignatura1,evaluacion1,porcentaje_evaluacion1);
+  values (id_evaluaciones.nextval,codigo_asignatura1,0,porcentaje_evaluacion1);
 END sp_evaluaciones;
 
 
