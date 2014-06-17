bffdfc746eab4ca3360b69318d908c4788c147c3
danielpunx1 <daniel_punx1@hotmail.com>
2014-06-17 00:33:15 -0600

    Agregando los proc y datos de las tablas

--- RegistroFIA/archivos sql/prerrequisito.sql
+++ RegistroFIA/archivos sql/prerrequisito.sql
@@ -10,3 +10,16 @@ BEGIN
     VALUES (id_prerrequisito.nextval,codigo_asignatura1,materia_rrequisito1);
 END sp_prerrequisito;
 
+execute sp_prerrequisito('FIR115','MAT115');
+execute sp_prerrequisito('FIR115','MTE115');
+execute sp_prerrequisito('HSE115','PSI115');
+execute sp_prerrequisito('MAT215','MAT115');
+execute sp_prerrequisito('MSM115','IAI115');
+execute sp_prerrequisito('PRN115','IAI115');
+execute sp_prerrequisito('MAT315','MAT215');
+execute sp_prerrequisito('MAT415','MAT315');
+execute sp_prerrequisito('FIR215','FIR115');
+execute sp_prerrequisito('FIR215','MAT215');
+execute sp_prerrequisito('FIR315','FIR215');
+execute sp_prerrequisito('FIR315','MAT215');
+execute sp_prerrequisito('FIR315','FIR115');
