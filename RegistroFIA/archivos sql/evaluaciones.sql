450b48e72b1a733e6a6c643edf5555ba3c9170c9
danielpunx1 <daniel_punx1@hotmail.com>
2014-06-17 02:01:43 -0600

    Agregando los proc y datos de las tablas

--- RegistroFIA/archivos sql/evaluaciones.sql
+++ RegistroFIA/archivos sql/evaluaciones.sql
@@ -15,7 +15,21 @@ BEGIN
 END sp_evaluaciones;
 
 
--- Asigna el nombre de la evaluacion es decir si es EV1 EV2 EV3
+-- Asigna el nombre de la evaluacion es decir si es 1 , 2 , 3 y no permite q se pase del 100 la sumatoria de los porcentajes
 
+create or replace trigger tr_evaluaciones_numer
+BEFORE INSERT ON evaluaciones
+FOR EACH ROW
+DECLARE
+total integer;
+porcentotal float;
+BEGIN
+    select count(*) into total from evaluaciones where CODIGO_ASIGNATURA = :new.CODIGO_ASIGNATURA;
+    :new.evaluacion := (total+1);
+    select sum(porcentaje_evaluacion) into porcentotal from evaluaciones where CODIGO_ASIGNATURA = :new.CODIGO_ASIGNATURA;
+    IF (porcentotal + :new.porcentaje_evaluacion) > 1.01 THEN
+         raise_application_error (-20600, 'Error el porcentaje supero el 100%. Porcentaje disponible : ' || ( :new.porcentaje_evaluacion - ((porcentotal + :new.porcentaje_evaluacion)-1)) || ' ' ); 
+    END IF;
+END;
 
 
